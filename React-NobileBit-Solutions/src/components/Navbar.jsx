793f23fc937e76c447f82c3e3ca4d219a789653f
majdawajneh <majdmahmoud003@gmail.com>
2026-02-17 16:34:50 +0300

    s

--- React-NobileBit-Solutions/src/components/Navbar.jsx
+++ React-NobileBit-Solutions/src/components/Navbar.jsx
@@ -2,17 +2,13 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
 return (
-<nav style={{ background: "#eee", padding: "10px" }}>
-{/* Use Link instead of <a> to avoid full page reload */}
-<Link to="/" style={{ marginRight: "10px" }}>
-Home
-    </Link>
+    <nav style={{ background: "#eee", padding: "10px" }}>
+    {/* Use Link instead of <a> to avoid full page reload */}
 
-        <Link to="/about" style={{ marginRight: "10px" }}>
-        About
-        </Link>
+    <Link to="/" style={{ marginRight: "10px", padding: "5px" }}>Home</Link>
+    <Link to="/about" style={{ marginRight: "10px", padding: "5px" }}>About</Link>
+    <Link to="/profile" style={{ padding: "5px" }}>Profile</Link>
 
-        <Link to="/profile">Profile</Link>
     </nav>
 );
 }
