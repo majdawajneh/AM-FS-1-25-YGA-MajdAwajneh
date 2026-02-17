545b505cd40aa6c1ddec492afc86c8b1a4727b60
majdawajneh <majdmahmoud003@gmail.com>
2026-02-17 17:26:59 +0300

    k

--- React-NobileBit-Solutions/src/Pages/Home.jsx
+++ React-NobileBit-Solutions/src/Pages/Home.jsx
@@ -1,7 +1,8 @@
+import React from 'react'
 function Home() {
 return (
-<div>
-<h1>Home Page</h1>
+<div className="text-center">
+    <h1>Home Page</h1>
 <p>Welcome to the home page.</p>
 </div>
 );
